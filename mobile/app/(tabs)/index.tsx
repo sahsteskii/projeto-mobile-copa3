9ffd2b0acb617b088174dac1e5469f32a68692de
seu-usuario-do-github <prado.samara@escola.pr.gov.br>
2025-10-10 11:59:38 -0300

    coloquei imagem tela de fundo

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -1,43 +1,31 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { ImageBackground, StyleSheet, View } from 'react-native';
 
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
 import { Link } from 'expo-router';
 
+const image = {uri: 'https://legacy.reactjs.org/logo-og.png'};
 export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/pele2.0.png')}
-          style={styles.reactLogo}
-        />
-      }>
+    <View
+    
+      >
+        <ImageBackground  source={require('@/assets/images/oii.png')}
+        resizeMode="cover" style={styles.image}>
+
+</ImageBackground>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title"></ThemedText>
+       
      
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle"></ThemedText>
-        <ThemedText>
-       {' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: '',
-            })}
-          </ThemedText>{' '}
         
-        </ThemedText>
+        
+        
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
         <Link href="/modal">
           <Link.Trigger>
-            <ThemedText type="subtitle"></ThemedText>
+        
           </Link.Trigger>
           <Link.Preview />
           <Link.Menu>
@@ -58,14 +46,13 @@ export default function HomeScreen() {
           </Link.Menu>
         </Link>
 
-        <ThemedText>
-        </ThemedText>
+        
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle"></ThemedText>
+      
        
       </ThemedView>
-    </ParallaxScrollView>
+    </View>
   );
 }
 
@@ -78,12 +65,11 @@ const styles = StyleSheet.create({
   stepContainer: {
     gap: 8,
     marginBottom: 8,
+  
   },
-  reactLogo: {
-    height: 400,
-    width: 378,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
+
+  image: {
+    height: '510%',
+    width: '100%'
+  }
 });

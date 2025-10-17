59eaef8e6c04ad20163b96905cfbc67bc4114a04
gigioo-00 <giovannagabriellyy001@gmail.com>
2025-10-17 10:25:02 -0300

    contato screen

--- mobile/app/(tabs)/grazy.tsx
+++ mobile/app/(tabs)/grazy.tsx
@@ -1,45 +1,31 @@
-import { Image } from 'expo-image';
-import { ImageBackground, Platform, StyleSheet, View } from 'react-native';
+import { ImageBackground, StyleSheet, View } from 'react-native';
 
-import { HelloWave } from '@/components/hello-wave';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
 import { Link } from 'expo-router';
 
-
-
 const image = {uri: 'https://legacy.reactjs.org/logo-og.png'};
-export default function HomeScreen() {
-
-
+export default function contatoscreen() {
   return (
+    
     <View
-      
+    
       >
+        <ImageBackground  source={require('@/assets/images/copagrazzy.png')}
+        resizeMode="cover" style={styles.image}>
+
+</ImageBackground>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">CUP MUNDIAL</ThemedText>
-        <HelloWave />
+       
+     
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">de 2010 a 2022</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
+        
+        
+        
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
         <Link href="/modal">
           <Link.Trigger>
-            <ThemedText type="subtitle">Step 2: Explore</ThemedText>
           </Link.Trigger>
           <Link.Preview />
           <Link.Menu>
@@ -60,20 +46,13 @@ export default function HomeScreen() {
           </Link.Menu>
         </Link>
 
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
+        
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
+      
+       
       </ThemedView>
+      
     </View>
   );
 }
@@ -87,12 +66,15 @@ const styles = StyleSheet.create({
   stepContainer: {
     gap: 8,
     marginBottom: 8,
+  
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
+
+  image: {
+    height: '520%',
+    width: '100%', 
+    opacity:  0.5
+  }
+  
 });
+
+
